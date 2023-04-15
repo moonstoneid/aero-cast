8edbdda2f4ed09522a5836ee4221a43c0d13c6d9
Michael Netter <mail@michaelnetter.com>
2023-04-15 15:41:02 +0200

    Add Sepolia testnet config

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,6 +1,17 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-toolbox");
+
+const ALCHEMY_URL = process.env.ALCHEMY_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.12",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
