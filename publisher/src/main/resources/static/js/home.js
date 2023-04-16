c051f11a379aa3a8f12f843958117aa0da68c7f5
Michael Netter <mail@michaelnetter.com>
2023-04-16 19:20:34 +0200

    Fix missing "await" statements in publisher frontend

--- publisher/src/main/resources/static/js/home.js
+++ publisher/src/main/resources/static/js/home.js
@@ -1,19 +1,15 @@
 import regContractMeta from "/abi/FeedRegistry.json" assert {type: "json"};
 import subContractMeta from "/abi/FeedSubscriber.json" assert {type: "json"};
 
-const subscribeButtonHandler = function () {
-    const connected = connectWallet();
-
-    if (connected) {
-        subscribe(PUB_CONTRACT_ADDRESS);
+const subscribeButtonHandler = async function () {
+    if (await connectWallet()) {
+        await subscribe(PUB_CONTRACT_ADDRESS);
     }
 }
 
-const unsubscribeButtonHandler = function () {
-    const connected = connectWallet();
-
-    if (connected) {
-        unsubscribe(PUB_CONTRACT_ADDRESS);
+const unsubscribeButtonHandler = async function () {
+    if (await connectWallet()) {
+        await unsubscribe(PUB_CONTRACT_ADDRESS);
     }
 }
 
@@ -23,16 +19,16 @@ const isSubscribedParagraph = document.getElementById('issubscribed-paragraph');
 subscribeButton && subscribeButton.addEventListener('click', subscribeButtonHandler, false);
 unsubscribeButton && unsubscribeButton.addEventListener('click', unsubscribeButtonHandler, false);
 
-document.addEventListener("DOMContentLoaded", function () {
-    const connected = connectWallet();
-    if (connected) {
-        if (isSubscribed(PUB_CONTRACT_ADDRESS)) {
-            setIsSubscribed();
-        } else {
-            setIsUnSubscribed();
-        }
-    } else {
+document.addEventListener("DOMContentLoaded", async function () {
+    if (!await connectWallet()) {
         // Show connect button
+        return;
+    }
+
+    if (await isSubscribed(PUB_CONTRACT_ADDRESS)) {
+        setIsSubscribed();
+    } else {
+        setIsUnSubscribed();
     }
 });
 
@@ -80,36 +76,36 @@ async function subscribe(pubContractAddr) {
 
 async function isSubscribed(pubContractAddr) {
     let subContractAddr;
-    let isSubscribed = false;
     try {
         const regContract = getRegContract();
         subContractAddr = await regContract.getSubscriberContract();
     } catch (error) {
         console.log("Could not get subscriber contract address: ", error);
-        return isSubscribed;
+        return false;
     }
     if (!subContractAddr) {
         console.log("No subscriber contract exists!");
-        return isSubscribed;
+        return false;
     }
 
+    let subscriptions;
     try {
         const subContract = getSubContract(subContractAddr);
-        const subscriptions = await subContract.getSubscriptions();
-
-
-        for (let s of subscriptions) {
-            if (s.pubAddress.toLowerCase() === pubContractAddr.toLowerCase()) {
-                isSubscribed = true;
-                break;
-            }
-        }
+        subscriptions = await subContract.getSubscriptions();
     } catch (error) {
         console.log("Could not get subs: ", error);
+        return false;
+    }
+
+    let isSubscribed = false;
+    for (let s of subscriptions) {
+        if (s.pubAddress.toLowerCase() === pubContractAddr.toLowerCase()) {
+            isSubscribed = true;
+            break;
+        }
     }
     return isSubscribed;
 }
-
 
 async function unsubscribe(pubContractAddr) {
     let subContractAddr;
